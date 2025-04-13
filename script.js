53aab724e7ce48c48a6e37f8b9854e3f12a8d178
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-13 20:15:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// your code here
+let heading = document.getElementById("url");
+let name  = document.getElementById("name");
+let year  = document.getElementById("year");
+let button  = document.getElementById("button");
+
+button.addEventListener("click", function(event){
+	event.preventDefault();
+
+	let nameR = name.value.trim();
+	let yearR = year.value.trim();
+	let baseUrl =  https://localhost:8080/
+	let params = [];
+	
+	if(nameR){
+		params.push(`name=${nameR}`)
+	}
+	if(yearR){
+		params.push(`year=${yearR}`)
+	}
+	
+})
